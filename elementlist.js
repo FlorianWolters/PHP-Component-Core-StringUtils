bfddb098585654100952e83591bb45a830049031
Florian Wolters <wolters.fl@gmail.com>
2013-06-29 14:18:00 +0200

    Updated API documentation to v0.3.0-stable.

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Core\\CharUtils"],["c","FlorianWolters\\Component\\Core\\RandomStringUtils"],["c","FlorianWolters\\Component\\Core\\StringUtils"],["c","FlorianWolters\\Component\\Core\\WordUtils"]];
+ApiGen.elements = [["c","Exception"],["c","FlorianWolters\\Component\\Core\\CharUtils"],["c","FlorianWolters\\Component\\Core\\RandomStringUtils"],["c","FlorianWolters\\Component\\Core\\StringEscapeUtils"],["c","FlorianWolters\\Component\\Core\\StringUtils"],["c","FlorianWolters\\Component\\Core\\WordUtils"],["c","InvalidArgumentException"],["c","LogicException"],["c","OutOfBoundsException"],["c","RuntimeException"]];
