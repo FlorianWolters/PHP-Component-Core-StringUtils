a5278adf8535f034ea451e2005192aeda2a1a894
Florian Wolters <wolters.fl@gmail.com>
2014-10-26 17:29:42 +0100

    Removed old files.
    Signed-off-by: Florian Wolters <wolters.fl@gmail.com>

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","FlorianWolters\\Component\\Core\\CharUtils"],["c","FlorianWolters\\Component\\Core\\RandomStringUtils"],["c","FlorianWolters\\Component\\Core\\StringEscapeUtils"],["c","FlorianWolters\\Component\\Core\\StringUtils"],["c","FlorianWolters\\Component\\Core\\WordUtils"],["c","InvalidArgumentException"],["c","LogicException"],["c","OutOfBoundsException"],["c","RuntimeException"]];
